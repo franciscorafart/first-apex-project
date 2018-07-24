67d331cff97a9934bb1cc6a9445e713a580a637a
Francisco Rafart <francisco.rafart@gmail.com>
2018-07-24 19:23:12 -0400

    write contacts to db

--- functions/writeDBcsv/index.js
+++ functions/writeDBcsv/index.js
@@ -1,6 +1,28 @@
+const AWS = require('aws-sdk')
+const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-2'})
+
 exports.handle = (e,ctx, cb) => {
-    console.log('e in writeDB', e)
-    //TODO: implement functionality
+    let params = {
+        'TableName': 'contacts'
+    }
+
+    e.forEach((el,idx) =>{
+        console.log('el '+idx+': '+el)
+
+        let name = el.name
+        let lastName = el.lastName
+        let phone = '1'+el.mobile
+
+        params['Item'] = {
+            name: name,
+            last_name: lastName,
+            phone: phone,
+        }
+
+        docClient.put(params, (err, data)=> {
+            err? cb(err, null): console.log('Wrote '+name+' to database')
+        })
+    })
 
     cb(null, 'Finished writing csv to database')
 }
