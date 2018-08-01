8e24a92c94b058047115a6e276e1c789e44f11d6
Francisco Rafart <francisco.rafart@gmail.com>
2018-07-31 22:12:13 -0400

    handle country code

--- functions/writeDBcsv/index.js
+++ functions/writeDBcsv/index.js
@@ -5,13 +5,14 @@ exports.handle = (e,ctx, cb) => {
     let params = {
         'TableName': 'contacts'
     }
+    //TODO: extract country information
 
     e.forEach((el,idx) =>{
         console.log('el '+idx+': '+el)
 
         let name = el.name
         let lastName = el.lastName
-        let phone = '1'+el.mobile
+        let phone = el.countryCode+el.mobile
 
         params['Item'] = {
             name: name,
