73363d720918edbecb1fbb0798c070cf5a33e490
Francisco Rafart <francisco.rafart@gmail.com>
2018-08-24 19:16:44 -0400

    delete functionality

--- functions/deleteContact/index.js
+++ functions/deleteContact/index.js
@@ -3,12 +3,16 @@ const AWS = require('aws-sdk')
 const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-2'})
 
 exports.handle = (e, ctx, cb) => {
-    console.log('e: ', e)
+    let phone = e.message.phone
+    console.log('phone', phone)
     let params = {
         TableName: 'contacts',
+        Key:{
+            "phone":phone
+        }
     }
-    // docClient.deleteItem(params, (err, data)=>{
-    //     err? cb(err, null): cb(null, data)
-    // })
-    cb(null, 'Ready!')
+    docClient.delete(params, (err, data)=>{
+        err? cb(err, null): cb(null, data)
+    })
+    // cb(null, 'Ready!')
 }
